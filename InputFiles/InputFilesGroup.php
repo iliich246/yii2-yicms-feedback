8e3ebf48ddedcffd8828392bad505d1907e4bfd5
iliich246 <iliich246@gmail.com>
2019-02-05 15:20:58 +0300

    Working on input handlers

---
+++ InputFiles/InputFilesGroup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zalupitto
+ * Date: 05.02.2019
+ * Time: 14:35
+ */
+
+namespace Iliich246\YicmsFeedback\InputFiles;
+
+
+class InputFilesGroup
+{
+
+}
---
+++ InputImages/InputImagesGroup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zalupitto
+ * Date: 05.02.2019
+ * Time: 14:45
+ */
+
+namespace Iliich246\YicmsFeedback\InputImages;
+
+
+class InputImagesGroup
+{
+
+}
